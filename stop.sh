77edc3a0a295fe437aefa1d19b96afd046348e4a
DMC <surendra.singh20.in@gmail.com>
2024-10-23 10:31:41 +0700

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. ~/.bash_profile  > /dev/null
+
+operator=$1
+
+script_name="./stop.sh"
+count=`ps -ef | grep $operator | grep -v grep | grep -v "$script_name" | wc -l`
+
+ if [ $count -eq 0 ]
+        then
+                echo "No Process Runing"
+        else
+        kill  $(ps aux | grep "$operator" | grep -v grep | grep -v "$script_name" | awk '{print $2}')
+        sleep 5
+	ps -ef | grep "$operator" | grep -v grep |  grep -v "$script_name" | awk '{print $2}' | xargs kill 
+	echo "Process $operator killed"
+ fi
+exit;
